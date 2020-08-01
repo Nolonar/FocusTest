2c1abb2fc132023023a0524e60cee64a46ef334f
Nolonar <leowyrsch@outlook.com>
2020-08-02 01:22:47 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FocusTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("test");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("test 2");
+            this.listViewProcesses = new System.Windows.Forms.ListView();
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderPID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listViewProcesses
+            // 
+            this.listViewProcesses.BackColor = System.Drawing.Color.Black;
+            this.listViewProcesses.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderPID,
+            this.columnHeaderTime});
+            this.listViewProcesses.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewProcesses.ForeColor = System.Drawing.Color.White;
+            this.listViewProcesses.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewProcesses.HideSelection = false;
+            this.listViewProcesses.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listViewProcesses.Location = new System.Drawing.Point(0, 0);
+            this.listViewProcesses.Name = "listViewProcesses";
+            this.listViewProcesses.Size = new System.Drawing.Size(800, 450);
+            this.listViewProcesses.TabIndex = 0;
+            this.listViewProcesses.UseCompatibleStateImageBehavior = false;
+            this.listViewProcesses.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "Process name";
+            // 
+            // columnHeaderPID
+            // 
+            this.columnHeaderPID.Text = "PID";
+            // 
+            // columnHeaderTime
+            // 
+            this.columnHeaderTime.Text = "Timestamp";
+            this.columnHeaderTime.Width = 75;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listViewProcesses);
+            this.Name = "MainForm";
+            this.Text = "Focus Tester";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewProcesses;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderTime;
+        private System.Windows.Forms.ColumnHeader columnHeaderPID;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FocusTest
+{
+    public partial class MainForm : Form
+    {
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const int WINEVENT_SKIPOWNPROCESS = 2;
+
+        private readonly WinEventDelegate evDelegate = null;
+        private readonly IntPtr eventHook = IntPtr.Zero;
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr UnhookWinEvent(IntPtr hWinEventHook);
+
+        private delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            evDelegate = new WinEventDelegate(OnForegroundWindowChanged);
+            eventHook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, evDelegate, 0, 0, WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            listViewProcesses.Items.Clear();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnhookWinEvent(eventHook);
+        }
+
+        private void OnForegroundWindowChanged(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            GetWindowThreadProcessId(hwnd, out int foregroundProcessId);
+            AddProcessToList(Process.GetProcessById(foregroundProcessId));
+        }
+
+        private void AddProcessToList(Process process)
+        {
+            string name = process.ProcessName;
+            if (string.IsNullOrWhiteSpace(process.MainWindowTitle) == false)
+                name += $" - {process.MainWindowTitle}";
+
+            var item = new ListViewItem(new string[] { name, process.Id.ToString(), DateTime.Now.ToLongTimeString() });
+            listViewProcesses.Items.Insert(0, item);
+
+            listViewProcesses.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.ColumnContent);
+            listViewProcesses.AutoResizeColumn(1, ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+    }
+}
